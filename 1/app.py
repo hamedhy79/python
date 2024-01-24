2ff80b4cb4c9c9119a5b37483b19292a8fb35483
hamed <hamedheydari7919@gmail.com>
2024-01-24 21:15:37 +0330

    pass.txt added

--- 1/app.py
+++ 1/app.py
@@ -52,20 +52,73 @@
 
 # print(msg_encrypted.lower())
 
+#6
+# msg_dict = {"Q":"A", "S:":"Q", "F":"G", "G":"T", "T":"G", "W":"B", "M":"H"}
+# msg_crypted = input("enter your crypted message: ").upper()
 
-msg_dict = {"Q":"A", "S:":"Q", "F":"G", "G":"T", "T":"G", "W":"B", "M":"H"}
-msg_crypted = input("enter your crypted message: ").upper()
+# msg_decrypted = ""
 
-msg_decrypted = ""
-
-for letters in msg_crypted:
-    if letters in msg_dict:
-        msg_decrypted += msg_dict[letters]
-    else:
-        msg_decrypted += letters
+# for letters in msg_crypted:
+#     if letters in msg_dict:
+#         msg_decrypted += msg_dict[letters]
+#     else:
+#         msg_decrypted += letters
         
-print(msg_decrypted.lower())
+# print(msg_decrypted.lower())
+
+#7
+# def add(a,b):
+#     return(a+b)
+# def zarb(a,b):
+#     return(a*b)
+# def divide(a,b):
+#     return(a/b)
+# def tafrig(a,b):
+#     return(a-b)
+
+# print("1 : add")
+# print("2 : zarb")
+# print("3 : divide")
+# print("4 : tafrig")
+
+# choice_num = input("please choice once number 1/2/3/4: ")
+
+# num_1 = int(input("please select your first num: "))
+# num_2 = int(input("please select your second num: "))
 
 
+# if choice_num == "1":
+#     print(num_1, "+" ,num_2, "=", add(num_1,num_2))
+# elif choice_num == "2":
+#     print(num_1, "*" ,num_2, "=", zarb(num_1,num_2))
+# elif choice_num == "3":
+#     print(num_1, "/" ,num_2, "=", divide(num_1,num_2))
+# elif choice_num == "4":
+#     print(num_1, "-" ,num_2, "=", tafrig(num_1,num_2))
 
+# else:
+#     print("not valid")
+
+#8
+import re
+p = input("Enter Your Password: ")
+
+x = True
+while x:
+    if (len(p)<6 or len(p)>16):
+        break
+    elif not re.search("[a-z]", p):
+        break
+    elif not re.search("[A-Z]", p):
+        break
+    elif not re.search("[0-9]", p):
+        break
+    elif not re.search("[!@#$]", p):
+        break
+    else:
+        print("Not Valid")
+        x = False
+        break
+if x:
+    print("Your Password Is Valid")
 
